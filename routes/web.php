461f1ae9b87ce1821255a33580cc2722c51807a1
GuysKapen <guyskapen@gmail.com>
2021-10-23 14:16:16 -0800

    Display user in home

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, "index"]);
 
 Route::get('/redirects', [HomeController::class, "redirects"]);
+
+Route::get('/users', [AdminController::class, "users"]);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
